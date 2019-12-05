32e6ff59d5d7037885224336f0f0587a09d1c82b
vladcatrinescu <vlad@vnext.solutions>
2019-12-05 14:22:01 +0100

    External Sharing Stuff

---
+++ External Sharing for Office 365/SiteCollectionExample.ps1
@@ -0,0 +1,28 @@
+﻿Set-SPOSite `
+	-Identity https://globomanticsorg.sharepoint.com/sites/DronesatGlobomantics `
+	-DefaultLinkPermission Edit `
+	-DefaultSharingLinkType Internal `
+	-SharingCapability ExternalUserAndGuestSharing  `
+	-ShowPeoplePickerSuggestionsForGuestUsers $false 
+
+Set-SPOSite `
+    -Identity https://globomanticsorg.sharepoint.com/sites/DronesatGlobomantics `
+    -SharingDomainRestrictionMode "AllowList" `
+    -SharingAllowedDomainList "globomantics.org"
+
+
+Get-SPOSite `
+    -IncludePersonalSite $true `
+    -Limit all `
+    -Filter "Url -like '-my.sharepoint.com/personal/'" | 
+    Select Url 
+
+Set-SPOSite `
+    -Identity https://globomanticsorg-my.sharepoint.com/personal/vlad_globomantics_org `
+     -SharingCapability Disabled
+
+Set-SPOSite `
+    -Identity https://globomanticsorg-my.sharepoint.com/personal/vlad_globomantics_org`
+     -SharingCapability ExternalUserAndGuestSharing
+
+#To See All Options > https://docs.microsoft.com/en-us/powershell/module/sharepoint-online/set-sposite?view=sharepoint-ps
---
+++ External Sharing for Office 365/TenantBCCandPolicy.ps1
@@ -0,0 +1,4 @@
+﻿Set-SPOTenant `
+	-BccExternalSharingInvitations $true `
+	-BccExternalSharingInvitationsList vlad@globomantics.org `
+	-CustomizedExternalSharingServiceUrl https://www.globomantics.org/sharingpolicies 
